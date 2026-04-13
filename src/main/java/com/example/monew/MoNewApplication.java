5f690de6ca0d3b5be62f3723f11f1f314d752ad9
seunghyeonjeon <seunghyeonjeon57@gmail.com>
2026-04-13 14:00:11 +0900

    feat:프로젝트 생성

---
+++ src/main/java/com/example/monew/MoNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.monew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoNewApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MoNewApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/monew/MoNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoNewApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
