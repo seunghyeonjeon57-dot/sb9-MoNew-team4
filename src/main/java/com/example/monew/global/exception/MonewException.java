73ef6576bcf9617694bd8c057475a297140cf94c
Newbress <75563523+Newbress@users.noreply.github.com>
2026-04-14 18:28:40 +0900

    Feat: add PostgresSql connection / schema.sql / .env / globalexception

---
+++ src/main/java/com/example/monew/global/exception/MonewException.java
@@ -0,0 +1,26 @@
+package com.example.monew.global.exception;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.Getter;
+
+@Getter
+public class MonewException extends RuntimeException{
+  private final Instant timestamp;
+  private final ErrorCode errorCode;
+  private final Map<String, Object> details;
+
+  public MonewException(ErrorCode errorCode, Map<String, Object> details) {
+    super(errorCode.getMessage());
+
+    this.timestamp = Instant.now();
+    this.details = details;
+    this.errorCode = errorCode;
+  }
+
+  public MonewException(ErrorCode errorCode) {
+    this(errorCode, new HashMap<>());
+  }
+
+}
