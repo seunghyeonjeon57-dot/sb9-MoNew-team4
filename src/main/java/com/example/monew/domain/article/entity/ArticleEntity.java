9a8b45bb498aa9d1557bb439b2ef732bfc266ed4
apple <apple@MacBookAir.Davolink>
2026-04-15 14:10:28 +0900

    MON-12 feat: ArticleEntity 구현

---
+++ src/main/java/com/example/monew/domain/article/entity/ArticleEntity.java
@@ -0,0 +1,83 @@
+package com.example.monew.domain.article.entity;
+
+import com.example.monew.global.base.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+
+@Entity
+@Getter
+@Table(name = "articles")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE articles SET is_deleted = true WHERE article_id = ?")
+@Where(clause = "is_deleted = false")
+public class ArticleEntity extends BaseEntity {
+
+  @Id
+  @GeneratedValue
+  @Column(name = "article_id", columnDefinition = "UUID")
+  private UUID id;
+
+  @Column(nullable = false, length = 100)
+  private String source;
+
+  @Column(name = "source_url", nullable = false, unique = true, length = 500)
+  private String sourceUrl;
+
+  @Column(nullable = false, length = 300)
+  private String title;
+
+  @Column(name = "publish_date")
+  private LocalDateTime publishDate;
+
+  @Column(columnDefinition = "TEXT")
+  private String summary;
+
+  @Column(name = "comment_count", nullable = false)
+  private long commentCount;
+
+  @Column(name = "view_count", nullable = false)
+  private long viewCount;
+
+
+  // 관심사 참조
+//  @ManyToOne(fetch = FetchType.LAZY)
+//  @JoinColumn(name = "interest_id")
+//  //private Interest interest;
+
+  //임시
+  private String interest;
+
+  @Builder
+  public ArticleEntity(String source, String sourceUrl, String title,
+      LocalDateTime publishDate, String summary, String interest) {
+    this.source = source;
+    this.sourceUrl = sourceUrl;
+    this.title = title;
+    this.publishDate = publishDate;
+    this.summary = summary;
+    this.interest = interest;
+    this.commentCount = 0;
+
+    this.viewCount = 0;
+  }
+
+  // 동일 사용자 체크 후 연산자로 1 증가
+  public void incrementViewCount() {
+    this.viewCount++;
+  }
+}
