9c4698921f8a81918a7c5744b2005ceb6f8145c8
seunghyeonjeon <seunghyeonjeon57@gmail.com>
2026-04-16 10:26:14 +0900

    MON-94 fix:builder와 Lombok충돌 해결 및 모니터링 수정

--- src/main/java/com/example/monew/domain/user/mapper/UserMapper.java
+++ src/main/java/com/example/monew/domain/user/mapper/UserMapper.java
@@ -1,17 +1,20 @@
 package com.example.monew.domain.user.mapper;
 
 
+import com.example.monew.domain.user.dto.UserDto;
 import com.example.monew.domain.user.dto.request.UserRegisterRequest;
 import com.example.monew.domain.user.entity.User;
+import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE
+,builder = @Builder(disableBuilder = true))
 public interface UserMapper {
-  @Mapping(target = "id" , ignore = true)
-  @Mapping(target= "createdAt", ignore = true)
   User toEntity(UserRegisterRequest request);
+
+  UserDto toDto(User user);
 
 
 
