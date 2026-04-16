c2d1f66edaf429a4fa474037e82fecaa819e70f0
촤건위 <chlrjsdnl@naver.com>
2026-04-16 15:31:52 +0900

    refactor: UUID 타입 변경

--- src/main/java/com/example/monew/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/example/monew/domain/notification/repository/NotificationRepository.java
@@ -1,10 +1,11 @@
 package com.example.monew.domain.notification.repository;
 
 import com.example.monew.domain.notification.entity.Notification;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
+public interface NotificationRepository extends JpaRepository<Notification, UUID> {
   // 지금은 기본 CRUD(save, findById 등)만 필요하므로 안은 비워둡니다!
 }
