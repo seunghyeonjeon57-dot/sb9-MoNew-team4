40b0ee6a67c737bddb60927c805fa606d706752d
apple <apple@MacBookAir.Davolink>
2026-04-17 20:30:56 +0900

    MON-98 feat&fix: ArticleView inCountView logic update & Test

--- src/main/java/com/example/monew/domain/article/service/ArticleViewService.java
+++ src/main/java/com/example/monew/domain/article/service/ArticleViewService.java
@@ -1,5 +1,52 @@
 package com.example.monew.domain.article.service;
 
+import com.example.monew.domain.article.dto.ArticleViewDto;
+import com.example.monew.domain.article.entity.ArticleEntity;
+import com.example.monew.domain.article.entity.ArticleViewEntity;
+import com.example.monew.domain.article.exception.ArticleNotFoundException;
+import com.example.monew.domain.article.mapper.ArticleMapper;
+import com.example.monew.domain.article.repository.ArticleRepository;
+import com.example.monew.domain.article.repository.ArticleViewRepository;
+import com.example.monew.global.exception.ErrorCode;
+import jakarta.transaction.Transactional;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ArticleViewService {
 
+  private final ArticleRepository articleRepository;
+  private final ArticleViewRepository articleViewRepository;
+
+  @Transactional
+  public ArticleViewDto logView(UUID articleId, UUID viewedBy, String clientIp) {
+    ArticleEntity article = articleRepository.findById(articleId)
+        .orElseThrow(() -> new ArticleNotFoundException(ErrorCode.ARTICLE_NOT_FOUND));
+
+    article.incrementViewCount(); // 기사 엔티티에
+
+    ArticleViewEntity viewRecord = ArticleViewEntity.builder()
+        .article(article)
+        .viewedBy(viewedBy)
+        .clientIp(clientIp)
+        .build();
+
+    ArticleViewEntity saved = articleViewRepository.save(viewRecord);
+
+    return ArticleViewDto.builder()
+        .id(saved.getId())
+        .viewedBy(saved.getViewedBy())
+        .createdAt(saved.getViewedAt())
+        .articleId(article.getId())
+        .source(article.getSource())
+        .sourceUrl(article.getSourceUrl())
+        .articleTitle(article.getTitle())
+        .articlePublishedDate(article.getCreatedAt())
+        .articleSummary(article.getSummary())
+        .articleViewCount(article.getViewCount())
+        .articleCommentCount(0L)
+        .build();
+  }
 }
