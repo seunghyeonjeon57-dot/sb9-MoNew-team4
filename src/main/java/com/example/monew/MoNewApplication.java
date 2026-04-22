d8223f521f7b97fed983554fb5a6e2623b22e876
Newbress <75563523+Newbress@users.noreply.github.com>
2026-04-22 15:05:39 +0900

    Refactor: MonewApplication EnableJpaRepositories 추가

--- src/main/java/com/example/monew/MoNewApplication.java
+++ src/main/java/com/example/monew/MoNewApplication.java
@@ -3,8 +3,10 @@ package com.example.monew;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.monew") // 갑자기 안되서 추가했습니다
 public class MoNewApplication {
   public static void main(String[] args) {
     SpringApplication.run(MoNewApplication.class, args);
