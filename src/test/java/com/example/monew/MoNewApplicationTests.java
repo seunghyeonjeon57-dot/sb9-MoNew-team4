2b8714b0b01785dd850634c34dd6819d2b8c5b10
촤건위 <chlrjsdnl@naver.com>
2026-04-23 18:20:50 +0900

    Fix: config, test

--- src/test/java/com/example/monew/MoNewApplicationTests.java
+++ src/test/java/com/example/monew/MoNewApplicationTests.java
@@ -1,16 +1,10 @@
 package com.example.monew;
 
-import com.example.monew.config.S3Config;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class MoNewApplicationTests {
-
-  @MockitoBean
-  private S3Config s3Config;
 
   @Test
   void contextLoads() {
