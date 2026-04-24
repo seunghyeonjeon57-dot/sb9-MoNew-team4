cabdaedae66a1187862a19ad51e2d22e1f7762e7
apple <apple@MacBookAir.Davolink>
2026-04-24 22:51:45 +0900

    MON-153 refactor: remove comment

--- src/test/java/com/example/monew/batch/BackupServiceTest.java
+++ src/test/java/com/example/monew/batch/BackupServiceTest.java
@@ -1,6 +1,5 @@
 package com.example.monew.batch;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -9,19 +8,16 @@ import static org.mockito.Mockito.*;
 import com.example.monew.domain.article.batch.service.BackupService;
 import com.example.monew.domain.article.batch.service.S3Service;
 import java.io.File;
-import java.io.IOException;
 import java.time.LocalDate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @ExtendWith(MockitoExtension.class)
 class BackupServiceTest {
